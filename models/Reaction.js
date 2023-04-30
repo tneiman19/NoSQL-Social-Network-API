be5bd57ea54cabbbafd9a3488d87ad24606fe99d
Tzvi (Steve) Neiman <tneiman19@gmail.com>
2023-04-30 19:37:30 -0400

    Added Thought Model and Reaction Schema

---
+++ models/Reaction.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const dateFormat = require("dateFormat");
+
+const { Schema, Types } = mongoose;
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp, "dddd, mmmm dS, yyyy, h:MM:ss TT"),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const Reaction = mongoose.model("Reaction", reactionSchema);
+
+module.exports = Reaction;
---
+++ models/Thought.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const dateFormat = require("dateFormat");
+
+const { Schema } = mongoose;
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp, "dddd, mmmm dS, yyyy, h:MM:ss TT"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Reaction",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model("Thought", thoughtSchema);
+
+module.exports = Thought;
